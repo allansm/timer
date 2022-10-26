92f29a8ea8a9a09b17ee979fae363b575bc168e2
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2022-10-26 17:01:44 -0300

    Pause on press enter

--- src/test2.cpp
+++ src/test2.cpp
@@ -8,57 +8,104 @@
 #endif
 
 #include <iostream>
+#include <thread>
+#include <vector>
 
-class Timer2{
+class Counter{
 	int time = 0;
 	int start;
 
+	std::vector<std::thread> loops;
+
+	bool pause = false;
+
+	void loop(){
+		while(pause){
+			#ifdef _WIN32
+				Sleep(1000);
+			#else
+				usleep(1000 * 1000);
+			#endif
+		}
+
+		struct timeval tp;
+		gettimeofday(&tp, NULL);
+		this->start = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+		
+		#ifdef _WIN32
+			Sleep(1000);
+		#else
+			usleep(1000 * 1000);
+		#endif
+
+		gettimeofday(&tp, NULL);
+
+		int time = tp.tv_sec * 1000 + tp.tv_usec / 1000 - this->start;
+		this->time += time;
+
+		this->loop();
+	}
+
+	void input_loop(){
+		std::string x;
+		getline(std::cin, x);
+
+		this->tap();
+		this->input();
+	}
+
+	void tap(){
+		this->pause = this->pause ? false : true;
+	}
+
 	public:
-		Timer2(int time=0){
+		Counter(int time=0){
 			this->time = time;
 
 			struct timeval tp;
 			gettimeofday(&tp, NULL);
 
 			start = tp.tv_sec * 1000 + tp.tv_usec / 1000;
+
+			loops.push_back(std::thread([this](){
+				this->loop();		
+			}));
 		}
 		
-		Time add(int ms=1000){
-			struct timeval tp;
-			gettimeofday(&tp, NULL);
-			this->start = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-			
-			#ifdef _WIN32
-				Sleep(ms);
-			#else
-				usleep(ms * 1000);
-			#endif
-
-			gettimeofday(&tp, NULL);
-
-			int time = tp.tv_sec * 1000 + tp.tv_usec / 1000 - this->start;
-			this->time += time;
-
+		Time current(){
 			int h = ((this->time/1000)/60) / 60;
 			int m = ((this->time/1000)/60) - (h*60);
 			int s = (this->time/1000) - ((m*60)+(h*60*60));
 
 			return {h, m, s, this->time};
 		}
+
+		void input(){
+			this->loops.push_back(std::thread([this](){
+				this->input_loop();		
+			}));
+		}
+
+		bool paused(){
+			return this->pause;
+		}
 };
 
-main(){
-	Timer2 t;
+int main(){
+	Counter counter;
+
+	counter.input();
 
 	while(true){
-		Time current = t.add(1000);
+		if(!counter.paused())
+			std::cout << counter.current().pattern() << "\n";
 
-		std::cout << current.pattern() << "\n";
-
-		if(current.ms % 2 == 0){
-			std::cout << "paused\n\n";
-			
-			usleep(1000*1000);
-		}
+		#ifdef _WIN32
+			Sleep(1000);
+		#else
+			usleep(1000 * 1000);
+		#endif	
 	}
+
+	return 0;
 }
