b773f2d896dabe173eb818753658c5d05942572e
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2022-11-05 18:27:26 -0300

    Edit constructor

--- include/timecache.hpp
+++ include/timecache.hpp
@@ -1,5 +1,9 @@
 #include <time.hpp>
 #include <fstream>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 class TimeCache{
 	std::string filename;
--- src/timecache.cpp
+++ src/timecache.cpp
@@ -1,6 +1,23 @@
 #include <timecache.hpp>
 
 TimeCache::TimeCache(std::string filename){
+	const char* home = getenv("HOME");
+	
+	home = home ? home : getenv("USERPROFILE");
+	chdir(home);
+	
+	struct stat info;
+	
+	if(stat("timer", &info) != 0){
+		#if defined _WIN32	
+			mkdir("timer");
+		#else
+			mkdir("timer", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+		#endif
+	}
+
+	chdir("timer");
+
 	this->filename = filename;
 }
 
